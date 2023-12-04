9ea8a344030b56465581b5c4816783a68cd42d1d
Kenneth <kennethlorenzg@gmail.com>
2023-12-03 23:20:02 -0500

    Add Add Book Functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const myLibrary = [];
-function Book(title, author, pages, haveRead) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.haveRead = haveRead; //add ternary? Boolean?
+  this.read = read; //add ternary? Boolean?
 }
 
 Book.prototype.info = function () {
@@ -16,6 +16,64 @@ const modalCloseBtn = document.querySelector(".close");
 const modal = document.querySelector(".modal");
 const addBookBtn = document.querySelector(".add");
 const modalContent = document.querySelector(".modal-content");
+const submitBtn = document.querySelector(".submit");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const form = document.querySelector("form");
+const read = document.querySelector("#read");
+const books = document.querySelector(".books");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const book = new Book(title.value, author.value, pages.value, read.checked);
+  myLibrary.push(book);
+  addBook(book);
+  closeModal();
+});
+
+function addBook(book) {
+  console.log(`From book card function ${book.title}`);
+  const card = document.createElement("div");
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const page = document.createElement("div");
+  const readBtn = document.createElement("button");
+  const removeBtn = document.createElement("button");
+
+  card.classList.add("book");
+  title.classList.add("title");
+  author.classList.add("author");
+  page.classList.add("page");
+  readBtn.classList.add(book.read ? "read" : "unread");
+  removeBtn.classList.add("remove");
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(page);
+  card.appendChild(readBtn);
+  card.appendChild(removeBtn);
+  title.textContent = book.title;
+  author.textContent = book.author;
+  page.textContent = book.pages;
+  readBtn.textContent = book.read ? "Read" : "Not Read";
+  removeBtn.textContent = "Remove";
+
+  books.append(card);
+}
+
+function resetForm() {
+  form.reset();
+}
+
+function closeModal() {
+  modalContent.classList.remove("modalZoom");
+  modalContent.classList.add("modalZoomOut");
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 200);
+  resetForm();
+}
 
 addBookBtn.addEventListener("click", () => {
   modal.style.display = "unset";
@@ -23,9 +81,5 @@ addBookBtn.addEventListener("click", () => {
   modalContent.classList.remove("modalZoomOut");
 });
 modalCloseBtn.addEventListener("click", () => {
-  modalContent.classList.remove("modalZoom");
-  modalContent.classList.add("modalZoomOut");
-  setTimeout(() => {
-    modal.style.display = "none";
-  }, 200);
+  closeModal();
 });
