c9e430b14abedf7d3df358f5da4d2fd10c40036e
kz <kennethlorenzg@gmail.com>
2026-02-02 21:07:34 -0500

    Add modal open / close functionalities

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const ADDBOOKBTN = document.getElementById("add-book");
+const ADDBOOKFORM = document.getElementById("addBookForm");
+const MODAL = document.querySelector(".modal");
+
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${read}`;
-  };
 }
+
+ADDBOOKBTN.addEventListener("click", () => {
+  MODAL.style.display = "flex";
+});
+
+window.onclick = function (e) {
+  if (e.target == MODAL) {
+    MODAL.style.display = "none";
+  }
+};
