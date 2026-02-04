1f9b9bd1be0bf46b42cfb4d52420ecf6db8e70bb
kz <kennethlorenzg@gmail.com>
2026-02-03 21:36:38 -0500

    Add functionalities when submitting form

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const ADDBOOKBTN = document.getElementById("add-book");
 const ADDBOOKFORM = document.getElementById("addBookForm");
 const MODAL = document.querySelector(".modal");
+const SUBMITFORMBTN = document.querySelector(`button[type="submit"]`);
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -20,3 +22,36 @@ window.onclick = function (e) {
     MODAL.style.display = "none";
   }
 };
+
+function clearForm() {
+  ADDBOOKFORM.reset();
+}
+
+function createBookObject() {
+  const id = crypto.randomUUID();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").checked;
+
+  const newBookObject = new Book(id, title, author, pages, read);
+  console.log(newBookObject);
+}
+
+function addBookToLibrary() {
+  const newBook = createBookObject();
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+}
+
+function hideModal() {
+  MODAL.style.display = "none";
+}
+
+ADDBOOKFORM.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  createBookObject();
+  clearForm();
+  hideModal();
+});
